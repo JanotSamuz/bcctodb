679271abef1339042d2e11bf39bfd44fd5d7dd79
Janot Samuz <janotsamuz+github@gmail.com>
2016-02-22 23:30:18 +0100

    Initialisation de la lecture d'un epub

--- bcctodb/src/main/java/com/janothome/bcctodb/App.java
+++ bcctodb/src/main/java/com/janothome/bcctodb/App.java
@@ -1,17 +1,15 @@
 package com.janothome.bcctodb;
 
-import com.google.gson.Gson;
-
 /**
- * Hello world!
+ * 
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello World!" );
-    	Gson gson = new Gson();
-        System.out.println(gson.toJson("Hello World!"));
+    	BCCEpub bccEpub = new BCCEpub();
+        System.out.println("Hello World!");
+        bccEpub.Read();
     }
 }
---
+++ bcctodb/src/main/java/com/janothome/bcctodb/BCCEpub.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.janothome.bcctodb;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.epub.EpubReader;
+
+/**
+ * @author PZGK7230
+ *
+ */
+public class BCCEpub {
+
+	String  BCCPath;
+
+	/**
+	 * @return the bccPath
+	 */
+	public String getBCCPath() {
+		return BCCPath;
+	}
+
+	/**
+	 * @param bccPath the bccPath to set
+	 */
+	public void setBCCPath(String BCCPath) {
+		this.BCCPath = BCCPath;
+	}
+
+	/**
+	 * 
+	 */
+	public BCCEpub() {
+		super();
+		this.BCCPath = "H:\\Documents\\Projet-BIBLE\\Bible_Crampon_1923_complete\\Bible_Crampon_1923_missionweb_upgraded.epub";
+	}
+	
+	public void Read() {
+		
+		// read epub file
+		EpubReader epubReader = new EpubReader();
+		try {
+			Book book = epubReader.readEpub(new FileInputStream(getBCCPath()));
+			// print the first title
+			List<String> titles = book.getMetadata().getTitles();
+			System.out.println("book title:" + (titles.isEmpty() ? "book has no title" : titles.get(0)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
